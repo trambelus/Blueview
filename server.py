707d998176a2880251669d161985c243687c1b92
Trambelus <trambelus2@gmail.com>
2016-05-08 20:11:30 -0400

    basestring for py2 compatibility

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ from datetime import timedelta
 from flask import make_response, request, current_app
 from functools import update_wrapper
 
-
+basestring = (str,bytes)
 def crossdomain(origin=None, methods=None, headers=None,
 				max_age=21600, attach_to_all=True,
 				automatic_options=True):
